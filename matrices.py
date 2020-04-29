de0536d48585c3fc2d884e2b65744d1dc00881f6
kieranrourke <64549749+kieranrourke@users.noreply.github.com>
2020-04-29 13:47:52 -0400

    adding code

---
+++ matrices.py
@@ -0,0 +1,91 @@
+def checkList (list):
+	## This was before I realized u could do j in range of i or list[i]
+	tempList = []
+	i = 1
+	j = 0
+	k = 0
+	for l in range(len(list)):    
+			try:
+				j = 0
+				while(i == 1):
+					list[l][j]
+					j += 1
+			except:
+				tempList.append(j)
+	for i in range(len(tempList)):
+		if j != tempList[i]:
+			return False
+	return True		
+
+def isNumeric (list):
+	k = 1
+	for i in range (len(list)):
+		for j in range (len(list[i])):		
+			check = isinstance (list[i][j], (int,float))
+			if check == False:
+				return False
+	return True
+
+def isMatrix (list):
+	checkMatrix = checkList(list)
+	checkNums = isNumeric(list)
+	if checkMatrix == False | checkNums == False:
+		return False
+	else:
+		return True
+
+def printMatrix (list):
+	isMatrix(list)
+	for i in range(len(list)):
+		for j in range(len(list[i])):
+			try:
+				print(list[i][j], end = '')	
+			except:
+				print("error")
+		print ("")
+
+
+def createZeroMatrix (n):
+	rows = []
+	for i in range(n):
+		columns = []
+		for j in range(n):
+			columns.append(0)
+		rows.append(columns)
+	return rows
+
+def identityMatrix (n):
+	matrix = createZeroMatrix(n)
+	for i in range(n):
+		for j in range(n):
+			if j == i:
+				matrix[i][j] = 1
+	return matrix
+def sameSize (matrix1, matrix2):
+	cols1 = 0
+	cols2 = 0 
+	rows1 = 0
+	rows2 = 0
+
+	for i in range(len(matrix1)):
+		rows1 += 1
+		for j in range (len(matrix1[i])):
+			cols1 += 1	
+	for i in range(len(matrix2)):
+		rows2 += 1
+		for j in range (len(matrix2[i])):
+			cols2 += 1	
+	print(cols1, cols2, rows1, rows2)
+	if cols1 == cols2 and rows1 == rows2 :
+		return True
+	else:
+		return False	
+
+
+
+
+myList = [[1,2], [2,3], [4,5]]
+myList2 = [[1,2], [2,3], [4,6,7]]
+matrix = identityMatrix(8)
+printMatrix(matrix)
+print(sameSize(myList, myList2))
