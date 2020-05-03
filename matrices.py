74a24897886ccd22fecd80a0c3c8e9fdb2546461
kieranrourke <64549749+kieranrourke@users.noreply.github.com>
2020-05-03 15:57:28 -0400

    updates

--- matrices.py
+++ matrices.py
@@ -1,20 +1,12 @@
 def checkList (list):
 	## This was before I realized u could do j in range of i or list[i]
-	tempList = []
-	i = 1
-	j = 0
-	k = 0
 	count = len(list[0])
-	for l in list    
-		tempCount = 0
-		for j in l:
-			tempCount += 1	
-		if tempCount != count:
+	for l in list:    
+		if len(l) != count:
 			return False	
 	return True		
 
 def isNumeric (list):
-	k = 1
 	for i in range (len(list)):
 		for j in range (len(list[i])):		
 			check = isinstance (list[i][j], (int,float))
@@ -23,23 +15,17 @@ def isNumeric (list):
 	return True
 
 def isMatrix (list):
-	checkMatrix = checkList(list)
-	checkNums = isNumeric(list)
-	if checkMatrix == False | checkNums == False:
-		return False
+	return(checkList(list)) and isNumeric(list)
+
+def printMatrix(list):
+	if(isMatrix(list)):
+		for row in list:
+			print("| ", end = '')
+			for value in row:
+				print(value, end=' ')
+			print("|")
 	else:
-		return True
-
-def printMatrix (list):
-	isMatrix(list)
-	for i in range(len(list)):
-		for j in range(len(list[i])):
-			try:
-				print(list[i][j], end = '')	
-			except:
-				print("error")
-		print ("")
-
+		print("error")
 
 def createZeroMatrix (n):
 	rows = []
@@ -57,28 +43,24 @@ def identityMatrix (n):
 			if j == i:
 				matrix[i][j] = 1
 	return matrix
+
 def sameSize (matrix1, matrix2):
-	cols1 = 0
-	cols2 = 0 
-	rows1 = 0
-	rows2 = 0
+	if not isMatrix(matrix1) or not isMatrix(matrix2):
+		return False
 
 	cols1 = len(matrix1[0])
 	rows1 = len(matrix1)
 	cols2 = len(matrix2[0])
 	rows2 = len(matrix2)
 	
-	
-	if cols1 == cols2 and rows1 == rows2 :
-		return True
-	else:
-		return False	
+	return cols1 == cols2 and rows1 == rows2	
 
 
 
 
 myList = [[1,2], [2,3], [4,5]]
-myList2 = [[1,2], [2,3], [4,6,7]]
+myList2 = [[1,2], [2,3], [4,6,5]]
 matrix = identityMatrix(8)
 printMatrix(matrix)
+print(isMatrix(myList2))
 print(sameSize(myList, myList2))
