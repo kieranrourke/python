65c7e1b6c831b14c914ed1f6d64fd4265de09a4d
kieranrourke <64549749+kieranrourke@users.noreply.github.com>
2020-05-14 14:40:39 -0400

    Update tutorial9.py

--- tutorial9.py
+++ tutorial9.py
@@ -9,42 +9,48 @@ def recurseMult(a,b):
     else:
         return a + recurseMult(a,b-1)
 
-def translate (string, morse, index):
+def translate (string, morse ):
     if index == len(string):
         return ''
-    if string[index] != ' ':
-       return morse[string[index]] + ' '  + translate(string,morse,index + 1)
+    if string[0] != ' ':
+       return morse[string[0]] + ' '  + translate(string[1::],morse)
         #morseString +=  morse[string[index]] 
         #return translate(string,morse,index + 1,morseString)
     else:
-        return ' ' + translate (string, morse, index + 1)
+        return ' ' + translate (string [1::],index + 1)
    
     
-def addList (nums, index):
-    if index == len(nums):
+def addList (nums):
+    if nums == []:
         return 0
     else:
-        return nums[index] + addList(nums, index + 1)
-def addList2 (nums, index, even, odd, zeroes):
-    if index == len(nums):
-        return 0
-    if nums[index] == 0:
-        return addList2(nums,index + 1,even, odd,zeroes + 1)
-    elif nums[index] % 2 == 0:
-        return addList2(nums,index + 1,even +nums[index], odd,zeroes)
-    else:
-        return addList2(nums,index + 1,even, odd +nums[index],zeroes)
-def minMax (myList, min,max, index):
-    if index == len(myList):
-        return max, min
-    if min > myList[index]:
-        return minMax(myList, myList[index], max, index + 1) 
-    elif max < myList[index]:
-        return minMax(myList, min, myList[index], index + 1) 
-    else:
-        return minMax(myList, min, max, index+1) 
-#leaving out last q
+        return nums.pop() + addList(nums)
 
+def addList2 (nums, even, odd, zeroes):
+    if nums == []:
+        return even,odd, zeroes
+    a = num.pop()
+    if a == 0:
+        return addList2(nums,index + 1,even, odd,zeroes + 1)
+    elif a % 2 == 0:
+        return addList2(nums,index + 1,even +a, odd,zeroes)
+    else:
+        return addList2(nums,index + 1,even, odd +a,zeroes)
+def addlist3 (nums):
+    addList2(nums,0,0,0)
+def minMax (myList, min,max):
+    if myList == []:
+        return max, min
+    a = myList.pop()
+    if min > a:
+        return minMax(myList, a, max) 
+    elif max < a:
+        return minMax(myList, min, a ) 
+    else:
+        return minMax(myList, min, max) 
+#leaving out last q
+def minMax1 (myList):
+    max, min = minMax(myList, myList[0], myList[0])
 MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
                     'C':'-.-.', 'D':'-..', 'E':'.', 
                     'F':'..-.', 'G':'--.', 'H':'....', 
@@ -66,7 +72,8 @@ myList = [1,2,3,4,5,6]
 min1 = 0
 max1 = 0
 index = 0
-max1, min1 = minMax(myList, min1,max1, index)
-
+max1, min1 = minMax(myList, min1,max1)
+print(translate("HI", MORSE_CODE_DICT))
+#print(addlist3(myList))
 #print (min1, max1)
 #print (addList(myList,0))
